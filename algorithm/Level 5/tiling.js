0dfdc61fdbaa7edf2750951cdedde8c7e81a1d8e
Park HyeongJin <jt3205@users.noreply.github.com>
2017-12-15 18:25:19 +0900

    2017-12-15

--- algorithm/Level 5/tiling.js
+++ algorithm/Level 5/tiling.js
@@ -1,8 +1,20 @@
-function tiling(n) {
-	var answer = 0;
+/*
+1	2	3	4	5	6
 
+1	2	3	5	8	13
+피보나치네
+*/
+function tiling(n) {
+	var answer = 1;
+	var num1 = 1;
+  var num2 = 1;
+  for(var i = 1; i < n; i++){
+  	num1 = num2;
+    num2 = answer;
+    answer = (num1+num2)%100000;		//마지막에만 나누려했는데 var 범위 넘어가는듯? 이상해짐
+  } 
 	return answer;
 }
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
-console.log(tiling(2));
+console.log(tiling(9));
